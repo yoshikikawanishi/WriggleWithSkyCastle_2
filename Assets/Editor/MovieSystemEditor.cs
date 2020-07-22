45c09a207fbb5db1cecee37bbbd087f8790a7de6
yoshikikawanishi <yoshikikawanishi449@gmail.com>
2020-07-23 01:58:06 +0900

    7/23
    MovieSystem

--- Assets/Editor/MovieSystemEditor.cs
+++ Assets/Editor/MovieSystemEditor.cs
@@ -7,7 +7,9 @@ using UnityEngine.Events;
 [CustomEditor(typeof(MovieSystem))]
 [CanEditMultipleObjects]
 public class MovieSystemEditor : Editor {
-    
+
+    public int function_Popup_Index = 0;
+
 
     public override void OnInspectorGUI() {
         
@@ -17,72 +19,127 @@ public class MovieSystemEditor : Editor {
 
         //イベント追加ボタン
         EditorGUILayout.BeginHorizontal();
-        GUILayout.FlexibleSpace();
-        if (GUILayout.Button("AddEvent", GUILayout.Width(160), GUILayout.Height(15))) {
-            obj.Add_Event(0);
+        {
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("AddEvent", GUILayout.Width(160), GUILayout.Height(15))) {
+                obj.Add_Event(0);
+            }
+            GUILayout.FlexibleSpace();            
         }
-        GUILayout.FlexibleSpace();
-        EditorGUILayout.EndHorizontal();        
+        EditorGUILayout.EndHorizontal();
+        GUILayout.Space(10);
 
         //イベントリスト        
         for (int i = 0; i < obj.list.Count; i++) {
             EditorGUILayout.BeginVertical(GUI.skin.box);
             
-            obj.list[i].name = EditorGUILayout.TextArea(obj.list[i].name);
-            obj.list[i].type = (MovieSystem.Event.Type)EditorGUILayout.EnumPopup("type", obj.list[i].type);           
+            //obj.list[i].name = EditorGUILayout.TextArea(obj.list[i].name);
+            obj.list[i].type = (MovieSystem.Event.Type)EditorGUILayout.EnumPopup("type", obj.list[i].type);
+
+            GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(1));
 
             //イベントごとの値
             switch (obj.list[i].type) {
                 case MovieSystem.Event.Type.message:                    
-                    obj.list[i].message.file_Name = EditorGUILayout.TextField("filename", obj.list[i].message.file_Name);
+                    obj.list[i].message.file_Name = EditorGUILayout.TextField("filename", obj.list[i].message.file_Name);                    
+                    obj.list[i].message.id = EditorGUILayout.Vector2IntField("ID", obj.list[i].message.id);
                     obj.list[i].message.dual_Panel = EditorGUILayout.Toggle("DualPanel", obj.list[i].message.dual_Panel);
-                    obj.list[i].message.start_ID = EditorGUILayout.IntField("startID", obj.list[i].message.start_ID);
-                    obj.list[i].message.end_ID = EditorGUILayout.IntField("endID", obj.list[i].message.end_ID);
                     obj.list[i].message.is_Auto = EditorGUILayout.Toggle("Auto Message", obj.list[i].message.is_Auto);
                     break;
                 case MovieSystem.Event.Type.motion:                    
-                    obj.list[i].motion.obj = (GameObject)EditorGUILayout.ObjectField("Object", obj.list[i].motion.obj, typeof(GameObject), true);
-                    obj.list[i].motion.x_Move = EditorGUILayout.CurveField("X Move", obj.list[i].motion.x_Move);
-                    obj.list[i].motion.y_Move = EditorGUILayout.CurveField("Y Move", obj.list[i].motion.y_Move);
-                    obj.list[i].motion.anim_Parameter = EditorGUILayout.TextField("Animation Parameter", obj.list[i].motion.anim_Parameter);
+                    obj.list[i].motion.obj = (Transform)EditorGUILayout.ObjectField("Object", obj.list[i].motion.obj, typeof(Transform), true);
+                    obj.list[i].motion.apply_Root_Position = EditorGUILayout.Toggle("ApplyRootPosition", obj.list[i].motion.apply_Root_Position);                    
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.PrefixLabel("Curve");
+                    EditorGUILayout.LabelField("X", GUILayout.Width(10));                    
+                    obj.list[i].motion.x_Move = EditorGUILayout.CurveField("", obj.list[i].motion.x_Move, GUILayout.Width(100));
+                    EditorGUILayout.LabelField("Y", GUILayout.Width(10));
+                    obj.list[i].motion.y_Move = EditorGUILayout.CurveField("", obj.list[i].motion.y_Move, GUILayout.Width(100));                    
+                    EditorGUILayout.EndHorizontal();
+                    break;
+                case MovieSystem.Event.Type.animation:
+                    obj.list[i].animation.kind = (MovieSystem.Animation.ParameterKind)EditorGUILayout.EnumPopup("Kind", obj.list[i].animation.kind);
+                    obj.list[i].animation.animator = (Animator)EditorGUILayout.ObjectField("Object", obj.list[i].animation.animator, typeof(Animator), true);
+                    obj.list[i].animation.parameter = EditorGUILayout.TextField("Parameter", obj.list[i].animation.parameter);
+                    if (obj.list[i].animation.kind == MovieSystem.Animation.ParameterKind.boolean)
+                        obj.list[i].animation.boolean = EditorGUILayout.Toggle("Bool", obj.list[i].animation.boolean);
+                    break;
+                case MovieSystem.Event.Type.localScale:
+                    obj.list[i].localScale.obj = (Transform)EditorGUILayout.ObjectField("Object", obj.list[i].localScale.obj, typeof(Transform), true);
+                    obj.list[i].localScale.kind = (MovieSystem.LocalScale.Kind)EditorGUILayout.EnumPopup("Kind", obj.list[i].localScale.kind);
+                    if(obj.list[i].localScale.kind == MovieSystem.LocalScale.Kind.direct) {
+                        obj.list[i].localScale.scale = EditorGUILayout.Vector2Field("Scale", obj.list[i].localScale.scale);
+                    }
+                    else {
+                        EditorGUILayout.BeginHorizontal();
+                        EditorGUILayout.PrefixLabel("Curve");
+                        EditorGUILayout.LabelField("X", GUILayout.Width(10));
+                        obj.list[i].localScale.x_Curve = EditorGUILayout.CurveField("", obj.list[i].localScale.x_Curve, GUILayout.Width(100));
+                        EditorGUILayout.LabelField("Y", GUILayout.Width(10));
+                        obj.list[i].localScale.y_Curve = EditorGUILayout.CurveField("", obj.list[i].localScale.y_Curve, GUILayout.Width(100));
+                        EditorGUILayout.EndHorizontal();
+                    }
                     break;
                 case MovieSystem.Event.Type.genObj:
                     obj.list[i].gen_Obj.obj = (GameObject)EditorGUILayout.ObjectField("Object", obj.list[i].gen_Obj.obj, typeof(GameObject), true);
                     obj.list[i].gen_Obj.parent = (GameObject)EditorGUILayout.ObjectField("Parent", obj.list[i].gen_Obj.parent, typeof(GameObject), true);
                     obj.list[i].gen_Obj.position = EditorGUILayout.Vector2Field("Position", obj.list[i].gen_Obj.position);
                     break;
-                case MovieSystem.Event.Type.function:                    
-                    obj.list[i].function.component =
-                        (MonoBehaviour)EditorGUILayout.ObjectField("Component", obj.list[i].function.component, typeof(MonoBehaviour), true
-                        );
-                    obj.list[i].function.function = EditorGUILayout.TextField("Function", obj.list[i].function.function
-                        );                    
+                case MovieSystem.Event.Type.function:
+                    GUILayout.BeginHorizontal();
+                    obj.list[i].function.obj = (GameObject)EditorGUILayout.ObjectField("", obj.list[i].function.obj, typeof(GameObject), true);                    
+                    if(obj.list[i].function.obj == null) {
+                        GUILayout.EndHorizontal();
+                        break;
+                    }                        
+                    MonoBehaviour[] components = obj.list[i].function.obj.GetComponents<MonoBehaviour>();
+                    if (components.Length == 0) {
+                        GUILayout.EndHorizontal();
+                        break;
+                    }
+                    string[] names = new string[components.Length];                    
+                    for (int j = 0; j < components.Length; j++)
+                        names[j] = components[j].ToString();
+                    function_Popup_Index = EditorGUILayout.Popup(function_Popup_Index, names);
+                    obj.list[i].function.component = components[function_Popup_Index];
+                    GUILayout.EndHorizontal();
+                    obj.list[i].function.function_Name = EditorGUILayout.TextField("", obj.list[i].function.function_Name);
                     break;
                 case MovieSystem.Event.Type.wait:                    
                     obj.list[i].wait.wait_Message = EditorGUILayout.Toggle("Wait End Message", obj.list[i].wait.wait_Message);
                     obj.list[i].wait.wait_Time = EditorGUILayout.FloatField("Time", obj.list[i].wait.wait_Time);
                     break;
-            }
+            }                       
 
-            //イベント削除ボタン  
+            //イベント並び替え・削除ボタン  
             EditorGUILayout.BeginHorizontal();
             GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Up", GUILayout.Width(80), GUILayout.Height(15)) ) {
+                obj.Swap_Event(i, i - 1);
+            }
+            if (GUILayout.Button("Down", GUILayout.Width(80), GUILayout.Height(15))) {
+                obj.Swap_Event(i, i + 1);
+            }
             if (GUILayout.Button("Remove", GUILayout.Width(80), GUILayout.Height(15))) {
                 obj.Remove_Event(i);
             }
             EditorGUILayout.EndHorizontal();
             
-            EditorGUILayout.EndVertical();            
+            EditorGUILayout.EndVertical();
+            GUILayout.Space(10);
 
             //追加ボタン
+            if (i >= obj.list.Count)
+                break;
             if (obj.list[i].type == MovieSystem.Event.Type.wait) {
                 EditorGUILayout.BeginHorizontal();
                 GUILayout.FlexibleSpace();
-                if (GUILayout.Button("AddEvent", GUILayout.Width(160), GUILayout.Height(20))) {
+                if (GUILayout.Button("AddEvent", GUILayout.Width(160), GUILayout.Height(15))) {
                     obj.Add_Event(i + 1);
                 }
                 GUILayout.FlexibleSpace();
                 EditorGUILayout.EndHorizontal();
+                GUILayout.Space(10);
             }
         
         }
@@ -90,7 +147,7 @@ public class MovieSystemEditor : Editor {
         //追加ボタン
         EditorGUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
-        if (GUILayout.Button("AddEvent", GUILayout.Width(160), GUILayout.Height(20))) {
+        if (GUILayout.Button("AddEvent", GUILayout.Width(160), GUILayout.Height(15))) {
             obj.Add_Event(obj.list.Count);
         }
         GUILayout.FlexibleSpace();
--- Assets/Scripts/Movie/MovieSystem.cs
+++ Assets/Scripts/Movie/MovieSystem.cs
@@ -6,42 +6,90 @@ using UnityEngine.Events;
 
 public class MovieSystem : MonoBehaviour {
 
+    /// <summary>
+    ///　ムービーの構成単位
+    ///　Typeで種類を選択
+    /// </summary>
     [System.Serializable]
     public class Event {
         public string name;
         public enum Type {
             message,
             motion,
+            animation,
+            localScale,
             genObj,
             function,
             wait,
         }
         public Type type = Type.message;
 
+        //それぞれのパラメータの箱を全部用意してるのはよくないけど、いい方法思いつかん
         public Message message = new Message();
         public Motion motion = new Motion();
+        public Animation animation = new Animation();
+        public LocalScale localScale = new LocalScale();
         public GenObject gen_Obj = new GenObject();
         public Function function = new Function();
         public Wait wait = new Wait();
     }
 
+    /// <summary>
+    /// メッセージ表示のパラメータ
+    /// </summary>
     [System.Serializable]
     public class Message {
         public string file_Name;
         public bool dual_Panel = true;
-        public int start_ID;
-        public int end_ID;
+        public Vector2Int id;
         public bool is_Auto;        
     }
 
+    /// <summary>
+    /// オブジェクトの移動用パラメータ
+    /// </summary>
     [System.Serializable]
     public class Motion {
-        public GameObject obj;
-        public string anim_Parameter;
+        public Transform obj;
+        public bool apply_Root_Position;
         public AnimationCurve x_Move = AnimationCurve.Constant(0, 1, 0);
         public AnimationCurve y_Move = AnimationCurve.Constant(0, 1, 0);
     }
 
+    /// <summary>
+    /// アニメーション変更のパラメータ
+    /// </summary>
+    [System.Serializable]
+    public class Animation {
+        public Animator animator;
+        public enum ParameterKind {
+            boolean,
+            trigger,
+        }
+        public ParameterKind kind;
+        public string parameter;
+        public bool boolean;
+    }
+   
+    /// <summary>
+    /// ローカルスケールの変更用パラメータ
+    /// </summary>
+    [System.Serializable]
+    public class LocalScale {
+        public Transform obj;
+        public enum Kind {
+            direct,
+            curve
+        }
+        public Kind kind;
+        public Vector2 scale;
+        public AnimationCurve x_Curve = AnimationCurve.Constant(0, 1, 1);
+        public AnimationCurve y_Curve = AnimationCurve.Constant(0, 1, 1);
+    }
+
+    /// <summary>
+    /// オブジェクト生成のパラメータ
+    /// </summary>
     [System.Serializable]
     public class GenObject {
         public GameObject obj;
@@ -49,27 +97,40 @@ public class MovieSystem : MonoBehaviour {
         public Vector2 position;
     }
 
+    /// <summary>
+    /// 関数呼び出しのパラメータ
+    /// </summary>
     [System.Serializable]
     public class Function {
+        public GameObject obj;
         public MonoBehaviour component;
-        public string function;
+        public string function_Name;
     }
 
+    /// <summary>
+    /// 待機のパラメータ
+    /// </summary>
     [System.Serializable]
     public class Wait {
         public bool wait_Message;
         public float wait_Time;
     }
 
+    //ムービー開始時に自機の操作を無効にするかどうか
     public bool is_Disable_Controlle = true;
+    //ムービーの構成単位をリストで並べて順次実行する
     public List<Event> list = new List<Event>();
 
-    private MessageDisplay _message;
-    private MessageDisplayCustom _message_Custom;    
+    //コンポーネント
+    private MessageDisplay _message;    //１パネルのメッセージ表示コンポーネント
+    private MessageDisplayCustom _message_Custom;      //２パネルのメッセージ表示用コンポーネント
+
+    //ムービー全体の終了を検知する用
     private bool is_End_Movie;
 
 
     void Awake() {
+        //取得
         _message = GetComponent<MessageDisplay>();
         _message_Custom = GetComponent<MessageDisplayCustom>();
         if(_message == null)
@@ -79,11 +140,15 @@ public class MovieSystem : MonoBehaviour {
     }
 
     
-    //ムービーを開始する
+    /// <summary>
+    /// ムービー開始
+    /// </summary>
     public void Start_Movie() {
         StartCoroutine("Movie_Cor");
     }
 
+
+    //ムービー本体
     private IEnumerator Movie_Cor() {
         //初期設定
         if (is_Disable_Controlle) {
@@ -99,14 +164,19 @@ public class MovieSystem : MonoBehaviour {
                     Start_Message(e);
                     break;
                 case Event.Type.motion:
-                    StartCoroutine("Object_Move_Cor", e);
-                    Play_Animation(e);
+                    StartCoroutine("Object_Move_Cor", e);                    
+                    break;
+                case Event.Type.animation:
+                    Change_Animation_Parameter(e);
+                    break;
+                case Event.Type.localScale:
+                    StartCoroutine("Change_Local_Scale_Cor");
                     break;
                 case Event.Type.genObj:
                     Generate_Object(e);
                     break;
                 case Event.Type.function:
-                    e.function.component.Invoke(e.function.function, 0);
+                    Invoke_Function(e);
                     break;
                 case Event.Type.wait:
                     if (e.wait.wait_Message) {                        
@@ -125,7 +195,8 @@ public class MovieSystem : MonoBehaviour {
         is_End_Movie = true;
     }
 
-    //ムービー終了
+
+    //外からムービー終了検知する用
     public bool End_Movie() {
         if (is_End_Movie) {
             is_End_Movie = false;
@@ -139,15 +210,15 @@ public class MovieSystem : MonoBehaviour {
     private void Start_Message(Event e) {
         if (e.message.is_Auto) {
             if(e.message.dual_Panel)
-                _message_Custom.Start_Display_Auto(e.message.file_Name, e.message.start_ID, e.message.end_ID, 1.0f, 0.02f);
+                _message_Custom.Start_Display_Auto(e.message.file_Name, e.message.id.x, e.message.id.y, 1.0f, 0.02f);
             else
-                _message.Start_Display_Auto(e.message.file_Name, e.message.start_ID, e.message.end_ID, 1.0f, 0.02f);
+                _message.Start_Display_Auto(e.message.file_Name, e.message.id.x, e.message.id.y, 1.0f, 0.02f);
         }
         else {
             if(e.message.dual_Panel)
-                _message_Custom.Start_Display(e.message.file_Name, e.message.start_ID, e.message.end_ID);
+                _message_Custom.Start_Display(e.message.file_Name, e.message.id.x, e.message.id.y);
             else
-                _message.Start_Display(e.message.file_Name, e.message.start_ID, e.message.end_ID);
+                _message.Start_Display(e.message.file_Name, e.message.id.x, e.message.id.y);
         }        
     }
 
@@ -162,41 +233,56 @@ public class MovieSystem : MonoBehaviour {
 
     //オブジェクトの移動を行う
     private IEnumerator Object_Move_Cor(Event e) {
-        GameObject obj = e.motion.obj;
+        Transform obj = e.motion.obj;
         AnimationCurve x_Curve = e.motion.x_Move;
         AnimationCurve y_Curve = e.motion.y_Move;
         if (x_Curve == null || y_Curve == null)
             yield break;
 
-        Vector2 root_Pos = obj.transform.position;
+        Vector2 root_Pos = Vector2.zero;
+        if(e.motion.apply_Root_Position)
+            root_Pos = obj.position;
         float xt = x_Curve.keys[x_Curve.keys.Length - 1].time;
         float yt = y_Curve.keys[y_Curve.keys.Length - 1].time;
         float end_Time = Mathf.Max(xt, yt);
 
-        for (float t = 0; t < end_Time; t += 0.016f + Time.deltaTime) {
-            obj.transform.position = root_Pos + new Vector2(x_Curve.Evaluate(t), y_Curve.Evaluate(t));
-            yield return new WaitForSeconds(0.016f);
+        for (float t = 0; t < end_Time; t += Time.deltaTime) {
+            obj.position = root_Pos + new Vector2(x_Curve.Evaluate(t), y_Curve.Evaluate(t));
+            yield return null;
         }
+    }    
+
+
+    //アニメーションパラメータの変更
+    private void Change_Animation_Parameter(Event e) {
+        Animator anim = e.animation.animator;
+        if(e.animation.kind == Animation.ParameterKind.boolean) {
+            anim.SetBool(e.animation.parameter, e.animation.boolean);
+        }
+        else {
+            anim.SetTrigger(e.animation.parameter);
+        }        
     }
 
-    //オブジェクトのアニメーション変更
-    //Bool型パラメータはBool, Trigger型パラメータはTriggerが付いていること限定
-    private void Play_Animation(Event e) {
-        if (e.motion.anim_Parameter == "")
-            return;
-        Animator anim = e.motion.obj.GetComponent<Animator>();
-        if (anim == null)
-            return;
-        
-        string parameter = e.motion.anim_Parameter;        
-        for (int i = 0; i < anim.parameters.Length; i++) {
-            string param = anim.parameters[i].name;            
-            if (param.Contains("Bool")) {
-                anim.SetBool(param, false);                
-            }
+
+    //ローカルスケール変更用
+    private IEnumerator Change_Local_Scale_Cor(Event e) {
+        Transform obj = e.localScale.obj;        
+        if(e.localScale.kind == LocalScale.Kind.direct) {
+            Vector2 scale = e.localScale.scale;
+            obj.localScale = new Vector3(scale.x, scale.y, 1);
         }
-        if (parameter.Contains("Bool")) {
-            anim.SetBool(parameter, true);
+        else {
+            AnimationCurve x_Curve = e.localScale.x_Curve;
+            AnimationCurve y_Curve = e.localScale.y_Curve;
+            float xt = x_Curve.keys[x_Curve.keys.Length - 1].time;
+            float yt = y_Curve.keys[y_Curve.keys.Length - 1].time;
+            float end_Time = Mathf.Max(xt, yt);
+
+            for (float t = 0; t < end_Time; t += Time.deltaTime) {
+                obj.localScale = new Vector3(x_Curve.Evaluate(t), y_Curve.Evaluate(t), 1);
+                yield return null;
+            }
         }
     }
 
@@ -209,13 +295,28 @@ public class MovieSystem : MonoBehaviour {
     }
 
 
+    //関数呼び出し
+    private void Invoke_Function(Event e) {
+        e.function.component.Invoke(e.function.function_Name, 0);
+    }
+
     #region EditorGUI用
     public void Add_Event(int index) {
         list.Insert(index, new Event());
-    }
+    }    
 
     public void Remove_Event(int index) {
         list.RemoveAt(index);
+    }
+
+    public void Swap_Event(int index_A, int index_B) {
+        if (index_A < 0 || index_B < 0)
+            return;
+        if (index_A > list.Count || index_B > list.Count)
+            return;
+        Event event_Tmp = list[index_A];
+        list[index_A] = list[index_B];
+        list[index_B] = event_Tmp;
     }
     #endregion
    
